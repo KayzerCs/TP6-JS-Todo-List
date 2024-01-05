4da7f5bfee3b543a3d84228bb1fd55ad56c70e91
Kayzer <max.mlv2210@gmail.com>
2024-01-05 13:42:48 +0100

    Exercice terminé

--- script.js
+++ script.js
@@ -1,31 +1,68 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const input = document.getElementById("item");
-  const form = document.querySelector("form");
-  const todoList = document.getElementById("list");
+// Exercice :
 
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
+// document.addEventListener("DOMContentLoaded", () => {
+//   const input = document.getElementById("item");
+//   const form = document.querySelector("form");
+//   const todoList = document.getElementById("list");
 
-    const newItemText = input.value.trim();
+//   form.addEventListener("submit", (e) => {
+//     e.preventDefault();
 
-    if (newItemText) {
-      addItem(newItemText);
-      input.value = "";
-    }
-  });
+//     const newItemText = input.value.trim();
 
-  const addItem = (text) => {
-    const listItem = document.createElement("li");
-    listItem.textContent = text;
+//     if (newItemText) {
+//       addItem(newItemText);
+//       input.value = "";
+//     }
+//   });
 
-    listItem.addEventListener("click", () =>
-      listItem.classList.toggle("checked")
-    );
-    listItem.addEventListener("contextmenu", (e) => {
-      e.preventDefault();
-      listItem.remove();
-    });
+//   const addItem = (text) => {
+//     const listItem = document.createElement("li");
+//     listItem.textContent = text;
 
-    todoList.appendChild(listItem);
-  };
+//     listItem.addEventListener("click", () =>
+//       listItem.classList.toggle("checked")
+//     );
+//     listItem.addEventListener("contextmenu", (e) => {
+//       e.preventDefault();
+//       listItem.remove();
+//     });
+
+//     todoList.appendChild(listItem);
+//   };
+// });
+
+// *****************************************************************************************************
+
+// Correctrion :
+
+const form = document.querySelector("form");
+
+function storeList() {
+  window.localStorage.todoList = list.innerHTML;
+}
+
+function getTodos() {
+  if (window.localStorage.todoList) {
+    list.innerHTML = window.localStorage.todoList;
+  } else {
+    list.innerHTML = `<li>Cliquez sur un todo pour le supprimer</li>`;
+  }
+}
+
+window.addEventListener("load", getTodos);
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  list.innerHTML += `<li>${item.value}</li>`;
+  item.value = "";
+});
+
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("checked")) {
+    e.target.remove();
+  } else {
+    e.target.classList.add("checked");
+  }
 });
